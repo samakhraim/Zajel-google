a2aebb9bca0f02ba1aa7ece7c73bdcb5d03678af
samakhraim <62477773+samakhraim@users.noreply.github.com>
2022-02-24 21:17:31 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var Url = "https://www.najah.edu/ar/";
+            Browser.Source = Url;
+        }
+
+
+
+        private void google_Clicked(object sender, EventArgs e)
+        {
+            var Url = "http://www.google.com/";
+            Browser.Source = Url;
+        }
+
+        private void zajel_Clicked(object sender, EventArgs e)
+        {
+            var Url = "https://zajel.najah.edu/";
+            Browser.Source = Url;
+        }
+    }
+
+
+
+}
